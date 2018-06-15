e460b29dcb7a5b02ff5d8c47b05f2814e6d055c9
Larsszz <flyerdnb@gmail.com>
2018-06-15 22:28:51 +0300

    New

---
+++ Lesson3_4/DigitsPrinter.java
@@ -0,0 +1,26 @@
+package Lesson3_4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+// Вывести в консоль цифры целого числа. Если число отрицательное, дополнять знаком «-».
+public class DigitsPrinter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int scannerNum = scanner.nextInt();
+
+        if (scannerNum < 0) {
+            scannerNum = scannerNum + (-scannerNum) + (-scannerNum);
+            System.out.print("- ");
+        }
+        int delitel = 1;
+        int[] arrayOfNum = new int[(int) Math.ceil(Math.log10(scannerNum + 0.5))];
+        for (int i = arrayOfNum.length - 1; i >= 0; i--) {
+            arrayOfNum[i] = (scannerNum / delitel) % 10;
+            delitel *= 10;
+        }
+        System.out.print(Arrays.toString(arrayOfNum));
+    }
+}
